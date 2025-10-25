3a05fac791bc9fa0814ac6e513ae5198349afaf7
darioanzaudo2017 <darioanzaudo@gmail.com>
2025-10-25 12:14:01 -0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -40,7 +40,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "19881f6f840da92c3e20ec4ebb54b97a
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "f9aba5c0f8273a960b38af5b853e5ec7",
 "/": "f9aba5c0f8273a960b38af5b853e5ec7",
-"main.dart.js": "ca629933e460be4bf00f2d9e69850d94",
+"main.dart.js": "8730bf0a67286f510c177819bcf6b0ab",
 "version.json": "1cfa9c4fc1dbcace7baa7efd0c9844e9"};
 // The application shell files that are downloaded before a service worker can
 // start.
